1ad4f111051b8812f328dfe3f77ff2e5d613dac3
SAM <samwfelice@gmail.com>
2022-08-04 20:17:06 +0530

    Add .babelrc file to fix jest bug

--- src/modules/itemsCounter.test.js
+++ src/modules/itemsCounter.test.js
@@ -1,2 +1,23 @@
 import itemsCounter from './itemsCounter.js';
+// import getShows from './getShows.js';
+// import addShow from './addShow.js';
 
+test('Count number of shows in the HomePage', () => {
+  document.body.innerHTML = `
+  <section class="shows-wrapper">
+  <div class="show"></div>
+  <div class="show"></div>
+  <div class="show"></div>
+  </section>
+  `;
+  // const loadShow = async () => {
+  //   let shows = [];
+  //   shows = await getShows();
+  //   shows = shows.filter((each, index) => index < 57);
+  //   shows.forEach((each) => {
+  //     addShow(each.id, each.name, each.image.medium);
+  //   });
+  // };
+  // loadShow();
+  expect(itemsCounter()).toHaveLength(3);
+});
